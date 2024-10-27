6cbc484b27bb95fbdbdb33b813e3918c0e506421
hodaifa-ech <echffani.hodaifa@etu.uae.ac.ma>
2024-10-27 14:29:25 +0100

    add goal Entity

---
+++ src/main/java/com/codeWithProjects/fitnessTrackerServer/services/goal/GoalService.java
@@ -0,0 +1,10 @@
+package com.codeWithProjects.fitnessTrackerServer.services.goal;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GoalService implements GoalServiceImpl {
+}
---
+++ src/main/java/com/codeWithProjects/fitnessTrackerServer/services/goal/GoalServiceImpl.java
@@ -0,0 +1,4 @@
+package com.codeWithProjects.fitnessTrackerServer.services.goal;
+
+public interface GoalServiceImpl {
+}
